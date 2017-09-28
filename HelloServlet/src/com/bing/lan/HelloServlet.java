cef205c41b319c1bb80cdc01a8e210baf0036079
dsj_lan_bing <lb@gdysj.com.cn>
2017-09-28 18:09:23 +0800

    update

--- HelloServlet/src/com/bing/lan/HelloServlet.java
+++ HelloServlet/src/com/bing/lan/HelloServlet.java
@@ -1,10 +1,16 @@
 package com.bing.lan;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet(value = "/hello/hello/hello", initParams = {@WebInitParam(name = "characterEncoding", value = "UTF-8")})
 public class HelloServlet extends HttpServlet {
 
     @Override
@@ -18,12 +24,21 @@ public class HelloServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        //response.addCookie(new Cookie("bing", "lan"));
+        HttpSession session = request.getSession();
+
+        session.setAttribute("bing","lan");
+
+        PrintWriter writer = response.getWriter();
+        writer.print("HelloServlet ----------");
+
+        System.out.println("HelloServlet.init: " + this);
     }
 }
--- HelloServlet/src/com/bing/lan/HttpServlet.java
+++ HelloServlet/src/com/bing/lan/HttpServlet.java
@@ -10,10 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public abstract class HttpServlet extends BaseServlet {
 
+    /**
+     * 针对于POST请求方式:
+     * request.setCharacterEncoding("UTF-8");
+     * 注意:1:只对POST有效,2:必须放在获取任意参数之前.
+     * 针对于GET请求方式:
+     * 修改Tocmcat中的server.xml配置文件中修改端口的元素,对GET方式的默认编码.
+     */
+
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding(getInitParameter("characterEncoding"));
+        response.setCharacterEncoding(getInitParameter("characterEncoding"));
+        response.setContentType("text/html");
         service(request, response);
     }
 
@@ -25,7 +36,7 @@ public abstract class HttpServlet extends BaseServlet {
         }
     }
 
-    protected abstract void doPost(HttpServletRequest request, HttpServletResponse response);
+    protected abstract void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException;
 
-    protected abstract void doGet(HttpServletRequest request, HttpServletResponse response);
+    protected abstract void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
